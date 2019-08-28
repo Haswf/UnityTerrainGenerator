2d00c7c262f079c2f9e01512a577dfee08cee349
Haswell Fan <haswell.fan@outlook.com>
2019-08-29 06:22:24 +1000

    incomplete dimond square algorithm

--- Project1/Assets/MeshGenerator.cs
+++ Project1/Assets/MeshGenerator.cs
@@ -10,49 +10,51 @@ public class MeshGenerator : MonoBehaviour
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
-    public int xSize = 20;
-    public int zSize = 20;
+    public int mSize = 5;
 
     //offset of the height.
-    public float mHeight;
-
+    public float seed = 0;
+    public float mHeight = 0;
+    
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         CreateShape();
+        DimondSquare();
         UpdateMesh();
+
     }
     
     void CreateShape()
     {
         // how many vertices
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        vertices = new Vector3[(mSize + 1) * (mSize + 1)];
 
         int i = 0;
-        for (int z = 0; z <= zSize; z++)
+        for (int z = 0; z <= mSize; z++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x <= mSize; x++)
             {
                 vertices[i] = new Vector3(x, 0, z);
                 i++;
             }
         }
         
-        triangles = new int[xSize * zSize * 6];
+        triangles = new int[mSize * mSize * 6];
 
         int vert = 0;
         int tris = 0;
-        for (int z = 0; z < zSize; z++)
+        for (int z = 0; z < mSize; z++)
         {
-            for (int x = 0; x < xSize; x++)
+            for (int x = 0; x < mSize; x++)
             {
                 triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 1] = vert + mSize + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
+                triangles[tris + 4] = vert + mSize + 1;
+                triangles[tris + 5] = vert + mSize + 2;
                 
                 vert++;
                 tris += 6;
@@ -63,46 +65,15 @@ public class MeshGenerator : MonoBehaviour
 
 
         //vertices[0].y = UnityEngine.Random.Range(-mHeight, mHeight);
-        //vertices[xSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
+        //vertices[mSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
         //vertices[vertices.Length - 1].y = UnityEngine.Random.Range(-mHeight, mHeight);
-        //vertices[vertices.Length - 1 - xSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
+        //vertices[vertices.Length - 1 - mSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
 
         //initial the height values for four vertexes of the origin square.
-        vertices[0].y = 5;
-        vertices[xSize].y = 5;
-        vertices[vertices.Length - 1].y = 5;
-        vertices[vertices.Length - 1 - xSize].y = 5;
-
-
-
-        //Iterations of using diamond_square algorithm of each square needs.
-        int iterations = (int)Mathf.Log(xSize, 2);
-        //Number of squares for each iteration
-        int numSquares = 1; 
-        //Edge length of square.
-        int squareSize = xSize; 
-
-        for (int n = 0; n < iterations; n++)
-        {
-            int row = 0;
-            for (int j = 0; j < numSquares; j++)
-            {
-                int col = 0;
-                for (int k = 0; k < numSquares; k++)
-                {
-                    DimondSquare(row, col, squareSize, mHeight);
-                    col += squareSize;
-                }
-                row += squareSize;
-            }
-            numSquares *= 2;
-            squareSize /= 2;
-            mHeight *= 0.5f;
-        }
-
-
-
-
+        vertices[0].y = seed;
+        vertices[mSize].y = seed;
+        vertices[vertices.Length - 1].y = seed;
+        vertices[vertices.Length - 1 - mSize].y = seed;
     }
 
     void UpdateMesh()
@@ -135,17 +106,60 @@ public class MeshGenerator : MonoBehaviour
     //Damiond square algorithm
     void DimondSquare(int row, int col, int size, float offset)
     {
-        int halfSize = (int)(size * 0.5f);
-        int topLeft = row * (xSize + 1) + col;
-        int botLeft = (row + size) * (xSize + 1) + col;
 
-        int mid = (int)(row + halfSize) * (xSize + 1) + (int)(col + halfSize);
-        vertices[mid].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[botLeft].y + vertices[botLeft + size].y) * 0.25f + UnityEngine.Random.Range(0, offset);
-        vertices[mid].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[botLeft + size].y) * 0.25f + UnityEngine.Random.Range(0, offset);
+        float average;
+        int VertexPerLine = mSize + 1;
+        // 2d arrays contains height of each vertex
+        float[,] heights = new float[VertexPerLine, VertexPerLine];
+        
+        // declare initial height of 
+        heights[0, VertexPerLine-1] = seed;
+        heights[0, 0] = seed;
+        heights[VertexPerLine-1, 0] = seed;
+        heights[VertexPerLine-1, VertexPerLine-1] = seed;
 
-        vertices[topLeft + halfSize].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
-        vertices[mid + halfSize].y = (vertices[topLeft].y + vertices[botLeft].y + vertices[mid].y) + UnityEngine.Random.Range(0, offset);
-        vertices[mid + halfSize].y = (vertices[topLeft + size].y + vertices[botLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
-        vertices[botLeft + halfSize].y = (vertices[botLeft].y + vertices[botLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
+        for (int sideLength = mSize; sideLength >= 2; sideLength /= 2)
+        {
+            int halfSize = sideLength / 2;
+            // x and y stand for coordinates of each vertex in 2d array 
+            for (int x = 0; x < VertexPerLine - 1; x += sideLength)
+            {
+                for (int y = 0; y < VertexPerLine - 1; y += sideLength)
+                {
+                    average = heights[x, y];
+                    average += heights[x + sideLength, y];
+                    average += heights[x, y + sideLength];
+                    average += heights[x + sideLength, y + sideLength];
+                    average /= 4.0f;
+
+                    average += UnityEngine.Random.Range(0, mHeight);
+                    heights[x + halfSize, y + halfSize] = average;
+                }
+            }
+
+            for (int x = 0; x < VertexPerLine - 1; x += halfSize)
+            {
+                for (int y = (x+halfSize) % sideLength; y < UPPER; y+=sideLength)
+                {
+                    
+                }
+            }
+        }
+
+
+//
+//
+//        int halfSize (int)(size * 0.5f);
+//        int topLeft = row * (mSize + 1) + col;
+//        int botLeft = (row + size) * (mSize + 1) + col;
+//
+//        int mid = (int)(row + halfSize) * (mSize + 1) + (int)(col + halfSize);
+//        vertices[mid].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[botLeft].y + vertices[botLeft + size].y) * 0.25f + UnityEngine.Random.Range(0, offset);
+//        vertices[mid].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[botLeft + size].y) * 0.25f + UnityEngine.Random.Range(0, offset);
+//
+//        vertices[topLeft + halfSize].y = (vertices[topLeft].y + vertices[topLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
+//        vertices[mid + halfSize].y = (vertices[topLeft].y + vertices[botLeft].y + vertices[mid].y) + UnityEngine.Random.Range(0, offset);
+//        vertices[mid + halfSize].y = (vertices[topLeft + size].y + vertices[botLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
+//        vertices[botLeft + halfSize].y = (vertices[botLeft].y + vertices[botLeft + size].y + vertices[mid].y) / 3 + UnityEngine.Random.Range(0, offset);
     }
 }
