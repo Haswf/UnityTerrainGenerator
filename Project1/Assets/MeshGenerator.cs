e802427d7ed14ebbc7060db05836e77c8150d59e
Haswell <shuyangf@student.unimelb.edu.au>
2019-08-31 13:59:45 +1000

    implemented smoothness mechanism to generate realistic terrain

--- Project1/Assets/MeshGenerator.cs
+++ Project1/Assets/MeshGenerator.cs
@@ -6,55 +6,70 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Mesh mesh;
+    // vector3 array of vertices
     private Vector3[] vertices;
-    private int[] triangles;
-    public int mSize = 5;
-
-    //offset of the height.
-    public float seed = 0;
-    public float mHeight = 0;
     
+    private int[] triangles;
+    
+    // range of height for generating height offset
+    public float range;
+    
+    // smotthness of the terrain. 
+    // 0 yields crystal world.
+    public float smoothness;
+    
+    // 2^n - 1. This is n.
+    public int dimension;
+    
+    // Assign random height to corners?
+    public bool randomCornerHeight;
+    
+    // 2^n - 1
+    private int size;
+
+    // Start is called before the first frame update
     void Start()
-    {
+    {    
+        // Create Mesh of size `size`
         mesh = new Mesh();
+        // Assign created mesh to the object attached to.
         GetComponent<MeshFilter>().mesh = mesh;
+        size = (int) Mathf.Pow(2, dimension) - 1;
         CreateShape();
         DiamondSquare();
         UpdateMesh();
-
     }
     
     void CreateShape()
     {
         // how many vertices
-        vertices = new Vector3[(mSize + 1) * (mSize + 1)];
+        vertices = new Vector3[(size + 1) * (size + 1)];
 
         int i = 0;
-        for (int z = 0; z <= mSize; z++)
+        for (int z = 0; z <= size; z++)
         {
-            for (int x = 0; x <= mSize; x++)
+            for (int x = 0; x <= size; x++)
             {
                 vertices[i] = new Vector3(x, 0, z);
                 i++;
             }
         }
         
-        triangles = new int[mSize * mSize * 6];
+        triangles = new int[size * size * 6];
 
         int vert = 0;
         int tris = 0;
-        for (int z = 0; z < mSize; z++)
+        for (int z = 0; z < size; z++)
         {
-            for (int x = 0; x < mSize; x++)
+            for (int x = 0; x < size; x++)
             {
                 triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + mSize + 1;
+                triangles[tris + 1] = vert + size + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + mSize + 1;
-                triangles[tris + 5] = vert + mSize + 2;
+                triangles[tris + 4] = vert + size + 1;
+                triangles[tris + 5] = vert + size + 2;
                 
                 vert++;
                 tris += 6;
@@ -62,18 +77,6 @@ public class MeshGenerator : MonoBehaviour
 
             vert++;
         }
-
-
-        //vertices[0].y = UnityEngine.Random.Range(-mHeight, mHeight);
-        //vertices[mSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
-        //vertices[vertices.Length - 1].y = UnityEngine.Random.Range(-mHeight, mHeight);
-        //vertices[vertices.Length - 1 - mSize].y = UnityEngine.Random.Range(-mHeight, mHeight);
-
-        //initial the height values for four vertexes of the origin square.
-        vertices[0].y = seed;
-        vertices[mSize].y = seed;
-        vertices[vertices.Length - 1].y = seed;
-        vertices[vertices.Length - 1 - mSize].y = seed;
     }
 
     void UpdateMesh()
@@ -82,90 +85,99 @@ public class MeshGenerator : MonoBehaviour
         mesh.vertices = vertices;
         mesh.triangles = triangles;
     }
-
-    private void OnDrawGizmos()
-    {
-        if (vertices == null)
-        {
-            return;
-        }
-
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Gizmos.DrawSphere(vertices[i], .1f);
-        }
+    
+    public void SetSeed(int seed) {
+        UnityEngine.Random.InitState(seed);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-
-
+    
     //Damiond square algorithm
     void DiamondSquare()
     {
         float average;
 
-        int DATA_SIZE = mSize + 2;
-        // 2d arrays contains height of each vertex
-        float[,] heights = new float[DATA_SIZE, DATA_SIZE];
+        // 2D array of height
+        float[,] heights = new float[size + 2, size + 2];
 
-        // declare initial height of 
-        heights[0,DATA_SIZE - 1] = seed;
-        heights[0, 0] = seed;
-        heights[DATA_SIZE - 1, 0] = seed;
-        heights[DATA_SIZE - 1, DATA_SIZE - 1] = seed;
-
-        for (int sideLength = DATA_SIZE-1; sideLength >= 2; sideLength /= 2)
+        // set seed for random number generator
+        SetSeed((int)UnityEngine.Random.Range(0, 100));
+        
+        // Assign random value within (-range, range) to each corner
+        if (randomCornerHeight)
+        {
+            heights[0,size+1] = UnityEngine.Random.Range(-range, range);
+            heights[0, 0] = UnityEngine.Random.Range(-range, range);
+            heights[size+1, 0] = UnityEngine.Random.Range(-range, range);
+            heights[size+1, size+1] = UnityEngine.Random.Range(-range, range);
+        }
+        // o/w assign range to each corner
+        else
+        {
+            heights[0,size+1] = range;
+            heights[0, 0] = range;
+            heights[size+1, 0] = range;
+            heights[size+1, size+1] = range;
+        }
+        
+        
+        for (int sideLength = size + 1; sideLength >= 2; sideLength /= 2)
         {
             int halfSize = sideLength / 2;
             // x and y stand for coordinates of each vertex in 2d array 
-            for (int x = 0; x < DATA_SIZE- 1; x += sideLength)
+            for (int x = 0; x < size + 1; x += sideLength)
             {
-                for (int y = 0; y < DATA_SIZE - 1; y += sideLength)
+                for (int y = 0; y < size+1; y += sideLength)
                 {
-                    average = heights[x, y];
-                    average += heights[x + sideLength, y];
-                    average += heights[x, y + sideLength];
-                    average += heights[x + sideLength, y + sideLength];
+                    average = heights[x, y]; // left top corner
+                    average += heights[x + sideLength, y]; // right top corner
+                    average += heights[x, y + sideLength]; // left bottom corner
+                    average += heights[x + sideLength, y + sideLength]; // left bottom corner
+                    
+                    // calc average
                     average /= 4.0f;
-
-                    average += UnityEngine.Random.Range(0, mHeight);
+                    // offset height by a random value
+                    average += UnityEngine.Random.value * (range * 2.0f) - range;
+                    // Assign new height to mid point
                     heights[x + halfSize, y + halfSize] = average;
                 }
             }
 
-            for (int x = 0; x < DATA_SIZE - 1; x += halfSize)
+            for (int x = 0; x < size+1; x += halfSize)
             {
-                for (int y = (x + halfSize) % sideLength; y < DATA_SIZE - 1; y += sideLength)
+                for (int y = (x + halfSize) % sideLength; y < size+1; y += sideLength)
                 {
-                    float avg = heights[(x - halfSize + DATA_SIZE - 1) % (DATA_SIZE - 1), y] +
-                        heights[(x + halfSize) % (DATA_SIZE - 1), y] +
-                        heights[x, (y + halfSize) % (DATA_SIZE - 1)] +
-                        heights[x, (y - halfSize + DATA_SIZE - 1) % (DATA_SIZE - 1)];
+                    average = heights[(x - halfSize + size+1) % (size+1), y];
+                    average += heights[(x + halfSize) % (size+1), y];
+                    average += heights[x, (y + halfSize) % (size+1)];
+                    average += heights[x, (y - halfSize + size+1) % (size+1)];
+                    
+                    // calc average
+                    average /= 4.0f;
+                    // offset height by a random value
+                    average += UnityEngine.Random.value * (range * 2.0f) - range;
 
-                    avg /= 4.0f;
-                    avg = avg + UnityEngine.Random.Range(0, mHeight);
+                    heights[x, y] = average;
 
-                    heights[x, y] = avg;
+                    if (x == 0)
+                    {
+                        heights[size+1, y] = average;
+                    }
 
-                    if (x == 0) heights[DATA_SIZE - 1, y] = avg;
-                    if (y == 0) heights[x, DATA_SIZE - 1] = avg;
-
+                    if (y == 0)
+                    {
+                        heights[x, size+1] = average;
+                    }
                 }
             }
+            // Lower the random value range
+            range -= range * 0.5f * smoothness;
         }
-        for (int x = 0; x < DATA_SIZE - 1; x++)
+        
+        // Map height to each vertex
+        for (int x = 0; x < size+1; x++)
         {
-            for (int y = 0; y < DATA_SIZE - 1; y++)
+            for (int y = 0; y < size+1; y++)
             {
-                //print(x);
-                //print(y);
-                //print(heights[x, y]);
-                vertices[x + y * (mSize + 1)].y = heights[x, y];
-
+                vertices[x + y * (size + 1)].y = heights[x, y];
             }
 
         }
